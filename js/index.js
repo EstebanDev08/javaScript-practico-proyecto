9e625f280ff2c766c402098af327ddabbc2eee50
Esteban <Elmejormaxsimus320@gmail.com>
2022-08-10 21:00:58 -0500

    agregamos el menu en mibile y corregimos errores varios

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
-let menu = document.querySelector('.options');
+const menu = document.querySelector('.options p');
 
-let menu_pegable = document.querySelector('.options-menu');
+const menu_pegable = document.querySelector('.options-menu');
+
+const menu_mobile = document.querySelector('.menu-mobile');
+
+const icon_menu = document.querySelector('.icon-menu');
 
 
 menu.addEventListener('click', () => activar_desactivar(menu_pegable));
-
+icon_menu.addEventListener('click', () => activar_desactivar(menu_mobile));
 
 
 
