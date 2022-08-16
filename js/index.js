9d1c1ac14f2b9a1c876fa0014404c24f9db62462
Esteban <Elmejormaxsimus320@gmail.com>
2022-08-16 15:53:59 -0500

    agregamos una pantalla oscura cuando abrimos un menu,todos los menus ce cirreran al tocar fuera de ellos, al tocar un produto, se abre en detalle de ese producto con su informacion

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ const menu_mobile = document.querySelector('.menu-mobile');
 
 const icon_menu = document.querySelector('.icon-menu');
 
+// que se oscuresca cuando se abra menu
+const dark_screen = document.querySelector('.dark-screen');
+
+
 /* menu opciones deskop  */
 
 const btn_menu = document.querySelector('.options p');
@@ -10,9 +14,8 @@ const menu_pegable = document.querySelector('.options-menu');
 
 btn_menu.addEventListener('click', function (){
 
-        activar_desactivar(menu_pegable);
-        menu_mycar.classList.add('inactive');
-        menu_mobile.classList.add('inactive');
+        cerrar_abrir_menus(menu_pegable);
+
 });
 
 
@@ -20,25 +23,19 @@ btn_menu.addEventListener('click', function (){
 
 const img_carshop = document.querySelector('.options img');
 
-const menu_mycar = document.querySelector('.product-detail');
+const menu_mycar = document.querySelector('.shoping-car-products');
 
 img_carshop.addEventListener('click', function (){
 
-            activar_desactivar(menu_mycar);
-            menu_pegable.classList.add('inactive');
-
-            menu_mobile.classList.add('inactive');
-
-} );
+            cerrar_abrir_menus(menu_mycar);
+      } );
 
 
-/* cerrar cariito de compras */
+/* cerrar cariito de compras  desde sui flecha*/
 const btn_orden_salir = document.querySelector('.title-container');
 
-btn_orden_salir.addEventListener('click', () => activar_desactivar(menu_mycar));
+btn_orden_salir.addEventListener('click', () => menu_mycar.classList.toggle('inactive'));
 
-
-/*cerrar menu mobile cuando abra el carrito*/
 
 
 
@@ -48,9 +45,8 @@ btn_orden_salir.addEventListener('click', () => activar_desactivar(menu_mycar));
 
 icon_menu.addEventListener('click', function (){
 
-    activar_desactivar(menu_mobile);
-    menu_mycar.classList.add('inactive');
-    menu_pegable.classList.add('inactive');
+    cerrar_abrir_menus(menu_mobile);
+
 
 
 });
@@ -62,82 +58,82 @@ let list_productos = [];
 list_productos.push({
     nombre: 'labial',
     precio: 50,
-    image:'/products_img/product1.jpg',
+    image:'./products_img/product1.jpg',
     categoria: 'electronics'
 });
 
 list_productos.push({
     nombre: 'labial',
     precio: 10,
-    image:'/products_img/product1.jpg',
-    categoria: 'furniture'
+    image:'./products_img/product1.jpg',
+    categoria: 'furnitures'
 });
 
 list_productos.push({
     nombre: 'labial',
     precio: 10,
-    image:'/products_img/product1.jpg',
+    image:'./products_img/product1.jpg',
     categoria: 'toys'
 });
 
 list_productos.push({
     nombre: 'labial',
     precio: 50,
-    image:'/products_img/product2.jpg',
+    image:'./products_img/product2.jpg',
     categoria: 'electronics'
 });
 
 list_productos.push({
     nombre: 'labial',
     precio: 10,
-    image:'/products_img/product1.jpg',
-    categoria: 'furniture'
+    image:'./products_img/product1.jpg',
+    categoria: 'furnitures'
 });
 
 list_productos.push({
     nombre: 'labial',
     precio: 10,
-    image:'/products_img/product1.jpg',
+    image:'./products_img/product1.jpg',
     categoria: 'toys'
 });
 list_productos.push({
     nombre: 'labial',
     precio: 50,
-    image:'/products_img/product1.jpg',
+    image:'./products_img/product1.jpg',
     categoria: 'electronics'
 });
 
 list_productos.push({
     nombre: 'labial',
     precio: 10,
-    image:'/products_img/product1.jpg',
-    categoria: 'furniture'
+    image:'./products_img/product1.jpg',
+    categoria: 'furnitures'
 });
 
 list_productos.push({
     nombre: 'labial',
     precio: 10,
-    image:'/products_img/product1.jpg',
+    image:'./products_img/product1.jpg',
     categoria: 'toys'
 });
 list_productos.push({
     nombre: 'labial',
     precio: 50,
-    image:'/products_img/product1.jpg',
+    image:'./products_img/product1.jpg',
     categoria: 'electronics'
 });
 
 list_productos.push({
     nombre: 'labial',
     precio: 10,
-    image:'/products_img/product1.jpg',
-    categoria: 'furniture'
+    image:'./products_img/product1.jpg',
+    categoria: 'furnitures'
 });
 
 list_productos.push({
     nombre: 'labial',
     precio: 10,
-    image:'/products_img/product2.jpg',
+    image:'./products_img/product2.jpg',
     categoria: 'toys'
 });
 
@@ -145,13 +141,14 @@ list_productos.push({
 /* llamamos funcion para que muestre los productos de todas las categorias*/
 mostrar_productos(list_productos, 'all');
 
+// abrir producto detail al hacer click en el producto
+let list_productos_container = document.querySelectorAll('.card-product');
 
 
 /*  mostrar productos por categorias  */
 
 /* obtengo a todos los objetos con clase btn_categoria */
 const btn_categoria = document.querySelectorAll('.btn-categoria');
-
 
 
 // recorro el array de todos los botones
@@ -161,23 +158,187 @@ btn_categoria.forEach(function (btn){
 
     btn.addEventListener('click', function (){
 
+
         let categoria = btn.querySelector('a').innerText.toLowerCase();
 
         console.log(categoria);
 
         mostrar_productos(list_productos, categoria);
+
+
+
+
+        menu_mobile.classList.add('inactive');
+
+
+        abrir_cargar_product_detail()
+
+
     });
 
 });
 
 
-/* funcion para activar o desactivar menu*/
-function  activar_desactivar ( elementoHtml){
 
-       elementoHtml.classList.toggle('inactive');
+// cerrar ventana detail y menus de productos al hacer click fuera de ellos
+
+
+document.addEventListener('click', function (e){
+
+    //contador para saber cuantos menus estan cerrados
+    let contador_menus_cerrados = 0;
+
+    console.log(e.target);
+
+//cuando toquemos la ventana oscura o el navbar cerramos los menus
+    if (e.target === dark_screen || e.target.nodeName === 'NAV' || e.target.classList.contains('product-detail-close')
+        || e.target === document.querySelector('.product-detail-close img')){
+
+        for (menu of list_menus) {
+
+            if (!menu.classList.contains('inactive')) {
+
+                menu.classList.add('inactive');
+
+                console.log("cerrar menus");
+            }
+
+        }
+    }
+
+
+
+
+
+// vemos si algun menu esta abierto y si lo esta activamos la ventana oscura y contamos los menus cerrados
+        for (menu of list_menus) {
+
+            if (!menu.classList.contains('inactive')) {
+                dark_screen.classList.remove('inactive');
+                console.log('dark screen activa');
+            }else {
+                contador_menus_cerrados++;
+            }
+
+
+        }
+
+
+
+    //si el contador de menus dice que todos los menus estan cerrados entonce desactivamos la ventana oscura
+
+
+    if (contador_menus_cerrados === list_menus.length) {
+
+        dark_screen.classList.add('inactive');
+        console.log('dark screen cerrado');
+    }
+
+
+
+} );
+
+
+//abrir produc detail
+
+const product_detail = document.querySelector('.product-detail-container');
+
+abrir_cargar_product_detail();
+
+
+
+
+
+
+
+
+//funcion para que en cada categoria se cargue el foreach con los productos nuevos y se pueda abrir el menu de detalles
+function abrir_cargar_product_detail(){
+
+
+    // abrir producto detail al hacer click en el producto
+    list_productos_container = document.querySelectorAll('.card-product');
+
+    list_productos_container.forEach(function (producto){
+
+        producto.addEventListener('click', function (){
+
+
+            list_productos_container = document.querySelectorAll('.card-product');
+
+
+
+            cerrar_abrir_menus(product_detail);
+
+            console.log(producto);
+
+
+
+            const img_producto = producto.querySelector('img').getAttribute('src');
+            const precio_producto = producto.querySelector('.product-info--card-product p').innerText;
+            const nombre_producto = producto.querySelector('.product-info--card-product p:nth-child(2)').innerText;
+
+
+            document.querySelector('.product-detail-container').innerHTML = `
+
+               <div class="product-detail">
+                   <div class="product-detail-close">
+                       <img src="./icons/icon_close.png" alt="close">
+                   </div>
+                   <img src="${img_producto}" alt="${nombre_producto}">
+                   <div class="product-info">
+                       <p>${precio_producto}</p>
+                       <p>${nombre_producto}</p>
+                       <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+                       <button class="primary-button add-to-cart-button">
+                           <img src="./icons/bt_add_to_cart.svg" alt="add to cart">
+                           Add to cart
+                       </button>
+                   </div>
+               </div>
+        `;
+
+        });
+
+    });
 
 }
 
+
+
+// funcios para que los menus despegables se cierren y se abran
+
+
+    const list_menus = document.querySelectorAll('.menu-despegable');
+
+
+
+    function cerrar_abrir_menus(menu_abrir) {
+
+
+
+        list_menus.forEach(function (menu){
+
+
+
+            if (menu !== menu_abrir){
+                menu.classList.add('inactive');
+
+
+            }else if( menu === product_detail){
+
+                menu.classList.remove('inactive');
+
+            }
+            else {
+                menu_abrir.classList.toggle('inactive');
+
+
+            }
+
+        } );
+
+    }
 
 
 /* funcion para mostar productos en html segun categoria*/
@@ -195,7 +356,7 @@ function mostrar_productos(productos, categoria){
             <div class="card-product">
                 <img src="${producto.image}" alt="${producto.nombre}">
                             
-                <div class="product-info">
+                <div class="product-info--card-product">
                 
                     <div>
                     
@@ -217,7 +378,7 @@ function mostrar_productos(productos, categoria){
             <div class="card-product">
                 <img src="${producto.image}" alt="${producto.nombre}">
                             
-                <div class="product-info">
+                <div class="product-info--card-product">
                 
                     <div>
                     
